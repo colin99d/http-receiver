19c7b20553547043c3fd7138edca8ac556331268
Colin Delahunty <colin99delahunty@gmail.com>
2025-01-17 21:32:50 -0500

    Added tests

--- src/arguments.rs
+++ src/arguments.rs
@@ -51,6 +51,21 @@ pub fn get_content_bytes(
     let clean = content?;
     if let Some(path) = clean.strip_prefix('@') {
         if let Ok(data) = std::fs::read(path) {
+            match encoding {
+                None => {
+                    if String::from_utf8(data.clone()).is_err() {
+                        println!("{}",  "Failed to decode file content as UTF-8, try adding a content encoding.".red());
+                    }
+                }
+                Some(inner_encoding) => {
+                    if inner_encoding.decode(&data).is_err() {
+                        println!(
+                            "{}",
+                            "Failed to decode file content, try a different encoding.".red()
+                        );
+                    }
+                }
+            }
             return Some(data);
         }
         let warning = format!("Failed to read file: {path}, sending the value '{clean}' instead.");
@@ -81,5 +96,82 @@ impl Args {
 
     pub const fn get_host(&self) -> IpAddr {
         self.host
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::handle_request;
+    use crate::requests::empty;
+    use crate::types::{ContentEncoding, ContentType, Header};
+    use http_body_util::BodyExt;
+    use hyper::{Request, StatusCode};
+    use std::io::Read;
+    use std::net::Ipv4Addr;
+    use tokio::runtime::Runtime;
+
+    async fn run_handle_request(config: &Config) -> (StatusCode, Vec<u8>, http::HeaderMap) {
+        let req = Request::builder()
+            .uri("http://localhost/test")
+            .body(empty())
+            .unwrap();
+
+        let response = handle_request(req, config).await.unwrap();
+        let status = response.status();
+        let headers = response.headers().clone();
+
+        let body_bytes = response.collect().await.unwrap().to_bytes().to_vec();
+        (status, body_bytes, headers)
+    }
+
+    fn test_encoding(encoding: ContentEncoding) {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            let original_text = "Check test".to_string();
+            let args = Args {
+                port: 9000,
+                host: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+                status_code: 200,
+                content: Some(original_text.clone()),
+                content_type: ContentType::Json,
+                content_encoding: Some(encoding.clone()),
+                headers: vec![],
+                highlight_headers: vec![],
+            };
+
+            let config = args.to_config();
+
+            let (status, body_bytes, headers) = run_handle_request(&config).await;
+
+            assert_eq!(status, StatusCode::OK);
+
+            let enc_header = headers.get("Content-Encoding").unwrap();
+            assert_eq!(enc_header.to_str().unwrap(), encoding.to_string());
+
+            println!("{:?}", body_bytes);
+            let encoded = encoding.encode(&original_text).unwrap();
+            assert_eq!(encoded, body_bytes);
+        });
+    }
+
+    #[test]
+    fn test_handle_request_gzip() {
+        test_encoding(ContentEncoding::Gzip);
+    }
+
+    #[test]
+    fn test_handle_request_deflate() {
+        test_encoding(ContentEncoding::Deflate);
+    }
+
+    #[test]
+    fn test_handle_request_br() {
+        test_encoding(ContentEncoding::Br);
+    }
+
+    #[test]
+    fn test_handle_request_zstd() {
+        test_encoding(ContentEncoding::Zstd);
     }
 }
